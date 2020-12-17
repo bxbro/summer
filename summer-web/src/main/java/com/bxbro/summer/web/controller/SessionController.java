7fd7a0df1e819ed0705a356b1ef54ea786cf5a9b
danieldong0802 <1756330108@qq.com>
2020-12-18 00:23:18 +0800

    [ADD]新增登錄接口（簡單版）後期完善

--- summer-web/src/main/java/com/bxbro/summer/web/controller/SessionController.java
+++ summer-web/src/main/java/com/bxbro/summer/web/controller/SessionController.java
@@ -1,23 +1,33 @@
 package com.bxbro.summer.web.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.bxbro.summer.common.entity.User;
+import com.bxbro.summer.common.resp.BaseResponse;
+import com.bxbro.summer.web.service.IUserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * @Description 用户注册/登录/登出/注销
  * @Author dong
  * @Date 2020/12/12
  */
-@Api(tags = "用户注册/登录/登出/注销")
+@Api(tags = "SessionController")
 @RestController
 @RequestMapping("/api/v1/session")
 public class SessionController {
 
+    @Autowired
+    private IUserService userService;
 
     /**
      *@Description 登录方法： 1.验证 2.设置session
@@ -26,8 +36,24 @@ public class SessionController {
      */
     @ApiOperation("用户登录")
     @PostMapping("/login")
-    public void login(HttpRequest request, HttpResponse response) {
+    public BaseResponse login(HttpServletRequest request, HttpServletResponse response) {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("user_name", username);
+        User user = userService.getOne(queryWrapper);
+        if (user == null) {
+            return BaseResponse.fail("該用戶不存在");
+        }
+        String passwordMd5 = DigestUtils.md5DigestAsHex(password.getBytes());
+        if (passwordMd5.equals(user.getPassword())) {
+            // 寫入session
+            HttpSession session = request.getSession();
 
+            return BaseResponse.successMsg("登錄成功");
+        }
+
+        return BaseResponse.fail("登錄失敗");
     }
 
 }
