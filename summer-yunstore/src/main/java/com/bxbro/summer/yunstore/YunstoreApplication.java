bb98381d5fdb08ed16a0717d8a79f9271029f958
danieldong0802 <1756330108@qq.com>
2021-03-01 23:26:26 +0800

    [ADD]新增yunstore模块：负责操作阿里云的tablestore

---
+++ summer-yunstore/src/main/java/com/bxbro/summer/yunstore/YunstoreApplication.java
@@ -0,0 +1,17 @@
+package com.bxbro.summer.yunstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dong
+ * @description TODO
+ * @date 2021/3/1
+ */
+@SpringBootApplication
+public class YunstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YunstoreApplication.class, args);
+    }
+}
---
+++ summer-yunstore/src/main/java/com/bxbro/summer/yunstore/config/YunStoreConfig.java
@@ -0,0 +1,12 @@
+package com.bxbro.summer.yunstore.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author dong
+ * @description TODO
+ * @date 2021/3/1
+ */
+@Configuration
+public class YunStoreConfig {
+}
