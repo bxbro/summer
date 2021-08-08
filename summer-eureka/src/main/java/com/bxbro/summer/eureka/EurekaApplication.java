e1275c3bd16bfa22ba74a1c26bb371ad962e3ed7
danieldong0802 <1756330108@qq.com>
2021-08-08 10:18:33 +0800

    add 引入eureka服务注册中心，由于spingboot 2.3.x与Finchley版本不兼容，因此用hoxton SR8版本

---
+++ summer-eureka/src/main/java/com/bxbro/summer/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.bxbro.summer.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author dong
+ * @description 服务注册中心
+ * @date 2021/8/8
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
