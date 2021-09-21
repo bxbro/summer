30ef0b80006df3a659067087e2c3c2c12055494c
danieldong0802 <1756330108@qq.com>
2021-09-21 21:13:07 +0800

    add 引入hystrix仪表盘

--- summer-web/src/main/java/com/bxbro/summer/web/WebApplication.java
+++ summer-web/src/main/java/com/bxbro/summer/web/WebApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
@@ -23,6 +24,7 @@ import org.springframework.web.client.RestTemplate;
 @MapperScan("com.bxbro.summer.web.mapper")
 @EnableScheduling
 @SpringCloudApplication
+@EnableHystrixDashboard
 public class WebApplication {
 
     public static void main(String[] args) {
